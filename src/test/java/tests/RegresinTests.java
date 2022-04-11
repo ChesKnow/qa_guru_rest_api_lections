5e9bfb82fd636cd2bdc28ec2a1a984c14bffbfc5
ChesKnow <Roman.Chesanov@outlook.com>
2022-04-11 22:57:25 +0300

    regres tests added

---
+++ src/test/java/tests/RegresinTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class RegresinTests {
+
+    @Test
+    void successfullLogin() {
+        given().body("{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}").contentType(ContentType.JSON)
+        .when().post("https://reqres.in/api/login")
+                .then().statusCode(200).body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+}
